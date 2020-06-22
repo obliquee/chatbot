bcc91f4db0cbd626e64b858cffc513f32b306012
https://github.com/obliquee <adhikarisameer2@gmail.com>
2020-06-22 17:30:20 +0545

    chat bot in flutter

---
+++ lib/main.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dialogflow/dialogflow_v2.dart';
+import 'package:bubble/bubble.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     home: ChatBot(),
+    );
+  }
+}
+
+class ChatBot extends StatefulWidget {
+  @override
+  _ChatBotState createState() => _ChatBotState();
+}
+
+class _ChatBotState extends State<ChatBot> {
+
+    void response(query) async{
+      print(query);
+    AuthGoogle authGoogle = await AuthGoogle(fileJson: "assets/chat-bot.json").build();
+    Dialogflow dialogflow =  Dialogflow(authGoogle: authGoogle, language: Language.english);
+    AIResponse aiResponse = await dialogflow.detectIntent(query);//await dialogflow.detectIntent(query);
+    
+    setState(() {
+      messages.insert(0,{
+        "data": 0,
+        "message": aiResponse.getListMessage()[0]["text"]["text"][0].toString()
+      });
+    });
+  }
+  final messageInsert = TextEditingController();
+  List<Map> messages = List();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar( 
+        title: Text("Home delivery Bot"),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+                child: ListView.builder(
+                    reverse: true,
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) => chat(
+                        messages[index]["message"].toString(),
+                        messages[index]["data"]))),
+            Divider(
+              height: 5.0,
+              color: Colors.deepOrange,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 15.0, right: 15.0),
+              margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                      child: TextField(
+                    controller: messageInsert,
+                    decoration: InputDecoration.collapsed(
+                        hintText: "Send your message",
+                        hintStyle: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18.0)),
+                  )),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 4.0),
+                    child: IconButton(
+                      
+                        icon: Icon(
+                          
+                          Icons.send,
+                          size: 30.0,
+                          color: Colors.deepOrange,
+                        ),
+                        onPressed: () {
+                          if (messageInsert.text.isEmpty) {
+                            print("empty message");
+                          } else {
+                            setState(() {
+                              messages.insert(0,
+                                  {"data": 1, "message": messageInsert.text});
+                            });
+                            response(messageInsert.text);
+                            messageInsert.clear();
+                          }
+                        }),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 15.0,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget chat(String message, int data) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Bubble(
+          radius: Radius.circular(15.0),
+          color: data == 0 ? Colors.deepOrange : Colors.orangeAccent,
+          elevation: 0.0,
+          alignment: data == 0 ? Alignment.topLeft : Alignment.topRight,
+          nip: data == 0 ? BubbleNip.leftBottom : BubbleNip.rightTop,
+          child: Padding(
+            padding: EdgeInsets.all(2.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage(
+                      data == 0 ? "assets/bot.png" : "assets/user.png"),
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Flexible(
+                    child: Text(
+                  message,
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ))
+              ],
+            ),
+          )),
+    );
+  }
+}
+
